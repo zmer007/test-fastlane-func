f71800f81ecf5385481188bae9730b28ee6808db
lgd <liguodong@zplay.cn>
2018-02-09 17:12:46 +0800

    Initial Commit

---
+++ TestFastlaneFunc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestFastlaneFunc
+//
+//  Created by lgd on 2018/2/9.
+//  Copyright © 2018年 playable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestFastlaneFunc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestFastlaneFunc
+//
+//  Created by lgd on 2018/2/9.
+//  Copyright © 2018年 playable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
